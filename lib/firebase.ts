05287656561d0f9f4d136f47e1d3de6c56b2a8ac
Muhammad zeeshan <fullstackdeveloper486@gmail.com>
2025-02-01 08:18:23 +0500

    resolve env file

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,7 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
-import "firebase/auth";
 import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -9,13 +8,9 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: "G-2FM3S6PCN9",
 };
 
-// Initialize Firebase
+// ✅ Ensure Firebase is initialized only once
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-
 export const auth = getAuth(app);
-
-auth.useDeviceLanguage();
 export default app;
